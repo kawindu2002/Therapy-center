0c0363094d08f7bc84c202ba6937a26abc3c37b7
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-08 14:15:30 +0530

    update session

---
+++ src/main/java/com/lk/kp/mentalhospital/controller/SessionBodyController.java
@@ -0,0 +1,189 @@
+//package com.lk.kp.mentalhospital.controller;
+//
+////import edu.ijse.gdse71.library.dto.UserDTO;
+////import edu.ijse.gdse71.library.dto.tm.UserTM;
+////import edu.ijse.gdse71.library.model.UserModel;
+////import edu.ijse.gdse71.library.util.CommonUtil;
+//
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.fxml.Initializable;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.ComboBox;
+//import javafx.scene.control.Label;
+//import javafx.scene.control.TextField;
+//import javafx.scene.input.MouseEvent;
+//import javafx.scene.layout.AnchorPane;
+//
+//import java.net.URL;
+//import java.sql.SQLException;
+//import java.util.ResourceBundle;
+//
+//public class SessionBodyController implements Initializable {
+//    @FXML
+//    private TableColumn<?, ?> costCol;
+//
+//    @FXML
+//    private Label costLbl;
+//
+//    @FXML
+//    private TextField costTxt;
+//
+//    @FXML
+//    private TableColumn<?, ?> dateCol;
+//
+//    @FXML
+//    private Label dateLbl;
+//
+//    @FXML
+//    private Label dateShowLbl;
+//
+//    @FXML
+//    private TableColumn<?, ?> durationCol;
+//
+//    @FXML
+//    private Label durationLbl;
+//
+//    @FXML
+//    private TextField durationTxt;
+//
+//    @FXML
+//    private TableColumn<?, ?> patientCol;
+//
+//    @FXML
+//    private ComboBox<?> patientIdCombo;
+//
+//    @FXML
+//    private Label patientNameLbl;
+//
+//    @FXML
+//    private Label patientNameShowLbl;
+//
+//    @FXML
+//    private Label patientidLbl;
+//
+//    @FXML
+//    private TableColumn<?, ?> programIdCol;
+//
+//    @FXML
+//    private ComboBox<?> programIdCombo;
+//
+//    @FXML
+//    private Label programIdLbl;
+//
+//    @FXML
+//    private Label programNameLbl;
+//
+//    @FXML
+//    private Label programNameShowLbl;
+//
+//    @FXML
+//    private AnchorPane sessionBodyAnchPane;
+//
+//    @FXML
+//    private TableView<?> sessionBodyTbl;
+//
+//    @FXML
+//    private Button sessionDeleteBtn;
+//
+//    @FXML
+//    private AnchorPane sessionHeaderAnchPane;
+//
+//    @FXML
+//    private TableColumn<?, ?> sessionIdCol;
+//
+//    @FXML
+//    private Label sessionIdLbl;
+//
+//    @FXML
+//    private Label sessionIdShowLbl;
+//
+//    @FXML
+//    private Label sessionManageLbl;
+//
+//    @FXML
+//    private Button sessionReportsBtn;
+//
+//    @FXML
+//    private Button sessionResetBtn;
+//
+//    @FXML
+//    private Button sessionSaveBtn;
+//
+//    @FXML
+//    private Button sessionUpdateBtn;
+//
+//    @FXML
+//    private TableColumn<?, ?> stateCol;
+//
+//    @FXML
+//    private ComboBox<?> stateCombo;
+//
+//    @FXML
+//    private Label stateLbl;
+//
+//    @FXML
+//    private TableColumn<?, ?> therapistIdCol;
+//
+//    @FXML
+//    private ComboBox<?> therapistIdCombo;
+//
+//    @FXML
+//    private Label therapistIdLbl;
+//
+//    @FXML
+//    private Label therapistNameLbl;
+//
+//    @FXML
+//    private Label therapistNameShowLbl;
+//
+//    @FXML
+//    void onClickTherapistTable(MouseEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void patientIdComboActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void programIdComboActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void sessionDeleteBtnActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void sessionReportsBtnActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void sessionResetBtnActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void sessionSaveBtnActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void sessionUpdateBtnActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void stateComboActionClicked(ActionEvent event) {
+//
+//    }
+//
+//    @FXML
+//    void therapistIdComboActionClicked(ActionEvent event) {
+//
+//    }
+//}
