b5f42cf1d936f8741ecca6b56545a1ee3094e70d
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-10 01:00:16 +0530

    add Dao and Bo

--- src/main/java/com/lk/kp/mentalhospital/bo/BoFactory.java
+++ src/main/java/com/lk/kp/mentalhospital/bo/BoFactory.java
@@ -1,4 +1,46 @@
 package com.lk.kp.mentalhospital.bo;
 
+//import com.lk.kp.mentalhospital.custom.impl.*;
+
+
 public class BoFactory {
+    private static BoFactory boFactory;
+    private BoFactory(){}
+
+    public static BoFactory getInstance() {
+        return boFactory==null?boFactory=new BoFactory():boFactory;
+    }
+
+    public enum BOTypes{
+        PATIENT,
+        PAYMENT,
+        PROGRAM,
+        REGISTRATION,
+        THERAPIST,
+        THERAPYSESSION,
+        USER
+
+    }
+
+    public SuperBO getBo(BoFactory.BOTypes boTypes){
+        switch (boTypes){
+
+            case PATIENT:
+//                return new PatientBoImpl();
+            case PAYMENT:
+//                return new PaymentBoImpl();
+            case PROGRAM:
+//                return new ProgramBoImpl();
+            case REGISTRATION:
+//                return new RegistrationBoImpl();
+            case THERAPIST:
+//                return new TherapistBoImpl();
+            case THERAPYSESSION:
+//                return new TherapySessionBoImpl();
+            case USER:
+//                return new UserBoImpl();
+            default:
+                return null;
+        }
+    }
 }
--- src/main/java/com/lk/kp/mentalhospital/dao/DaoFactory.java
+++ src/main/java/com/lk/kp/mentalhospital/dao/DaoFactory.java
@@ -1,4 +1,44 @@
 package com.lk.kp.mentalhospital.dao;
 
 public class DaoFactory {
+    private static DaoFactory daoFactory;
+    private DaoFactory(){}
+
+    public static DaoFactory getInstance() {
+        return daoFactory==null?daoFactory=new DaoFactory():daoFactory;
+    }
+
+    public enum DAOTypes{
+        PATIENT,
+        PAYMENT,
+        PROGRAM,
+        REGISTRATION,
+        THERAPIST,
+        THERAPYSESSION,
+        USER
+
+    }
+
+
+    public SuperDAO getDao(DAOTypes daoTypes){
+        switch (daoTypes){
+
+            case PATIENT:
+//                return new PatientDaoImpl();
+            case PAYMENT:
+//                return new PaymentDaoImpl();
+            case PROGRAM:
+//                return new ProgramDaoImpl();
+            case REGISTRATION:
+//                return new RegistrationDaoImpl();
+            case THERAPIST:
+//                return new TherapistDaoImpl();
+            case THERAPYSESSION:
+//                return new TherapySessionDaoImpl();
+            case USER:
+//                return new UserDaoImpl();
+            default:
+                return null;
+        }
+    }
 }
