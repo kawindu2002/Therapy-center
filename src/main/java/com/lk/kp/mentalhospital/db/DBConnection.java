9ea354aa897c5ca70c61b9d9889cf938ba98b0d9
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-18 19:31:31 +0530

    update db

--- src/main/java/com/lk/kp/mentalhospital/db/DBConnection.java
+++ src/main/java/com/lk/kp/mentalhospital/db/DBConnection.java
@@ -13,9 +13,9 @@ public class DBConnection {
     private final Connection connection;
 
     private DBConnection() throws SQLException {
-        String URL = "jdbc:mysql://localhost:3306/Supermarketfx?createDatabaseIfNotExist=true";
+        String URL = "jdbc:mysql://localhost:3306/serenity_therapy";
         String USER = "root";
-        String PASSWORD = "1234";
+        String PASSWORD = "1234";jdbc:mysql://localhost:3306/serenity_therapy
         connection = DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
