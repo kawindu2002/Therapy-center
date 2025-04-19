3fed2c5b2288911bc36eb27fbb2d894e26ceffdd
kawindu2002 <kawindupriyashan@gmail.com>
2025-04-19 18:36:10 +0530

    update crud dao

--- src/main/java/com/lk/kp/mentalhospital/dao/CrudDAO.java
+++ src/main/java/com/lk/kp/mentalhospital/dao/CrudDAO.java
@@ -3,7 +3,18 @@ package com.lk.kp.mentalhospital.dao;
 import com.lk.kp.mentalhospital.entity.SuperEntity;
 
 public interface CrudDAO<T extends SuperEntity, ID> extends SuperDAO {
+
+//    public boolean save(T t);
+//
+//    public boolean update(T t);
+//
+//    public boolean deleteByPK(ID pk) throws Exception;
+//
+//    public List<T> getAll();
+//
+//    public Optional<T> findByPK(ID pk);
+//
+//    public Optional<String> getLastPK();
+
 }
-
-
 
